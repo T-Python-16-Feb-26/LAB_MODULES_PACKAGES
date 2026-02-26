7b93adee6f11f50e8d567851da5783e9c177d411
FadhelAlmalki <almalkifadhel@gmail.com>
2026-02-26 12:21:05 +0300

    Added my solution

---
+++ main.py
@@ -0,0 +1,36 @@
+import dateOP
+from library import librarian
+
+# For Question 1
+dateOP.print_current_date()
+print()
+
+# For Bonus
+library = {}
+
+librarian.add_book(library, 
+                   "The Catcher in the Rye", 
+                   "J.D. Salinger", 
+                   "9780316769174")
+
+librarian.add_book(library, 
+                   "To Kill a Mockingbird", 
+                   "Harper Lee", 
+                   "9780446310789")
+
+librarian.add_book(library, 
+                   "1984", 
+                   "George Orwell", 
+                   "9780451524935")
+
+librarian.display_books(library)
+print()
+
+librarian.check_out_book(library, "9780316769174")
+
+librarian.display_books(library)
+print()
+
+librarian.return_book(library, "9780316769174")
+
+librarian.display_books(library)
