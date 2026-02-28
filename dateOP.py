f814cbc73323d44ae1166d1fe6488a6c8482436e
Rimas Alshahrani <remasmushbab@gmail.com>
2026-02-28 16:10:04 +0300

    # add solution for lab_modules_packages and bonus

---
+++ dateOP.py
@@ -0,0 +1,7 @@
+# import datetime class
+from datetime import datetime
+
+# function to print current date and time
+def print_current_date():
+    current_date = datetime.now()
+    return f"The current time is: {current_date}"
